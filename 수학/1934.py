c2a76cc4450add5f7519248b1bf77486d21de6fb
gaeun <geun5744@gmail.com>
2022-10-14 15:52:36 +0900

    1934번 해결

--- 수학/1934.py
+++ 수학/1934.py
@@ -1,25 +1,9 @@
-num = int(input())
-result=[]
+T = int(input())
+for _ in range(T):
+    A, B = map(int, input().split())
+    result = A*B
 
-for i in range(num):
+    while B>0:
+        A,B = B, A%B
 
-  a,b=map(int,input().split())
-  k=[]
-  total=1
-
-  
-  for j in range(1,max(a+1,b+1)):
-    if((a%j==0) and (b%j==0)):
-      k.append(j)
-      a = a//j
-      b = b//j
-      
-  for l in range(len(k)):
-    total*=k[l]
-  
-  total*=a*b
-
-  result.append(total)
-
-for i in range(len(result)):
-  print(result[i])
+    print(result//A)
