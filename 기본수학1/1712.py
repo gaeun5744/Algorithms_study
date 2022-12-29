bd8a8ae56ed0c11b78d961aca8bd09c40539b987
gaeun <geun5744@gmail.com>
2022-12-30 00:11:14 +0900

    1712.py 성공

--- 기본수학1/1712.py
+++ 기본수학1/1712.py
@@ -1,11 +1,13 @@
 A, B, C = map(int,input().split())
 
-n=0
 
 if B>=C:
   print(-1)
+else:
+  n=int(A/(C-B))
+  if A+B*n<C*n:
+    print(n)
+  else:
+    print(n+1)
 
-while A+B*n>=C*n:
-  n+=1
 
-print(n)
