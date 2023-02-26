ecd7bd0490ae34cfbedae28defa50a99c04ab024
gaeun <geun5744@gmail.com>
2023-02-26 23:34:13 +0900

    1193

--- 기본수학1/1193.py
+++ 기본수학1/1193.py
@@ -3,14 +3,20 @@ N=int(input())
 kan=1
 gap=0
 
-while N>kan:
+while N>=kan:
   gap+=1
   kan+=gap
 
 kan-=gap
 tap=N-kan
-print(str(1+gap-tap)+"/"+str(1+tap))
 
+print(kan,gap)
+
+
+if gap%2==0:
+  print(str(1+tap)+"/"+str(gap-tap))
+else:
+  print(str(gap-tap)+"/"+str(1+tap))
 
 
 
