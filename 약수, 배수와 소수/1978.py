33700b2f5ff28de0b8f087cfd4964613ae6dc5ab
gaeun <geun5744@gmail.com>
2023-03-11 23:41:23 +0900

    [Math]create 1978.py

---
+++ 약수, 배수와 소수/1978.py
@@ -0,0 +1,24 @@
+In=[]
+result=[]
+while True:
+  n=int(input())
+  if n==-1:
+    break
+  else:
+    In.append(n)
+
+for num in In:
+  peo=[]
+  total=0
+  for i in range(1,num):
+    if num%i==0:
+      peo.append(i)
+  for j in peo:
+    total+=j
+  if total==num:
+    print(num,"=", end=" ")
+    for i in range(len(peo)-1):
+      print(peo[i],"+",end=" ")
+    print(peo[-1])
+  else:
+    print(num,"is NOT perfect")
