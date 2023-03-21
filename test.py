215769f15dd862855a9f16ce1465db3b3f8c0c86
gaeun <geun5744@gmail.com>
2023-03-21 16:10:48 +0900

    [Greedy]create 1이 될 때까지.py

--- test.py
+++ test.py
@@ -1,2 +1,19 @@
-for i in range(2,2):
-    print(i)
+N,K=map(int,input().split())
+count=0
+
+while(True):
+  if (N%K==0):
+    break
+  else:
+    N-=1
+    count+=1
+
+while(True):
+  N=N//K
+  count+=1
+  if(N==1):
+    break
+
+print(count)
+
+  
